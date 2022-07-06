37bf75c1b0a33b0abceda2db0a3aad9e8bd39157
anroMK <marek_1308@tlen.pl>
2022-07-06 14:42:35 +0200

    06.07.22

---
+++ 6kyu/break_camelCase.py
@@ -0,0 +1,16 @@
+"""
+Complete the solution so that the function will break up camel casing, using a space between words.
+
+Example
+"camelCasing"  =>  "camel Casing"
+"identifier"   =>  "identifier"
+""             =>  ""
+
+"""
+
+import re
+
+def solution(s):
+    return re.sub(r'(\w)([A-Z])', r'\1 \2', s)
+
+print(solution("camelCasing" ))
